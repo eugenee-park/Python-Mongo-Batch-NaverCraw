297788d5e9192edaa6efea11286568ee5cb3d726
engenee-park <98isabella0416@gmail.com>
2021-09-28 14:03:12 +0900

    파이썬에 뉴스데이터 시각화하기 - 폴링까지!

--- news_test.py
+++ news_test.py
@@ -1,6 +1,16 @@
 import requests
 
 url = "http://localhost:8080/news"
-response = requests.get(url)
 
-print(response.text)
+# response.json()
+# json문자열 => json.loads() => dict
+# dict => json.dumps() => json
+
+response = requests.get(url)
+newsDto = response.json();
+
+if newsDto["code"] == 1:
+    #print(type(newsDto["data"]))
+    print(newsDto["data"][0])
+
+
